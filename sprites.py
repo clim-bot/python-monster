b308d8184022cb4ae4cf09cedf9864bf9493a061
Christopher Lim <toffer.lim87@gmail.com>
2025-02-19 11:00:03 -0800

    - add data, graphics and some initial code

---
+++ sprites.py
@@ -0,0 +1,7 @@
+from settings import *
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, group):
+        super().__init__(group)
+        self.image = surf
+        self.rect = self.image.get_frect(topleft = pos)
